87483e8c80ab33051387d1a3ef51017e2afd3013
francomekus <francomekus@gmail.com>
2022-09-10 18:56:36 -0700

    added 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; num_base--)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
