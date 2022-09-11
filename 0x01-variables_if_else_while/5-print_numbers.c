4cb93833e77a836a9083449c24b76968a5ead847
francomekus <francomekus@gmail.com>
2022-09-10 18:36:24 -0700

    added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num_base;
+
+	for (num_base = 0; num_base <= 9; num_base++)
+		printf("%d", num_base);
+	printf("\n");
+
+	return (0);
+}
