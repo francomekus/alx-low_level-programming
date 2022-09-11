f6c79a4d30189722df0f80eb8b574a8b62917fca
francomekus <francomekus@gmail.com>
2022-09-10 19:12:20 -0700

    added 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alpha;
+	int num;
+	
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
